b2afa00930ca361c5c2469c6a9da3cc495e3ff52
Tarandeep Singh <128288715+Tarandeep9988@users.noreply.github.com>
2024-04-07 14:49:02 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,48 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Tarandeep",
+            lastname: "Singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2);
+
+const obj3 = {...obj1, ...obj2} // 90 % of the time it is used
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 2,
+        email: "h2@gmail.com"
+    },
+]
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLogged"))
